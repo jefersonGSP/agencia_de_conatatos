f0c5df6e2c5cfa49da112c5258eaaf0a6643833d
Jeferson G.S. Peixoto <seth616@outlook.com.br>
2021-08-11 19:07:17 -0300

    atulizando

---
+++ lib/provider/listOfContacts.dart
@@ -0,0 +1,48 @@
+import 'package:agenda_de_contatos/models/contact.dart';
+
+List<Contact> listOfContacts = [
+  Contact(
+      name: "Nicole Costa",
+      photo: "assets/images/Nicole.jpg",
+      email: "nicole.costa@gmail.com",
+      phone: "51 11111-1111"),
+  Contact(
+      name: "Camila Zigofreda",
+      photo: "assets/images/Camila.jpg",
+      email: "camila@gmail.com",
+      phone: "51 22222-2222",
+      isFavorite: true),
+  Contact(
+      name: "Claudia Fagundes",
+      photo: "assets/images/Claudia.jpg",
+      email: "claudia@gmail.com",
+      phone: "51 33333-3333"),
+  Contact(
+      name: "Juliana dos Lixos",
+      photo: "assets/images/Juliana.jpg",
+      email: "juliana@gmail.com",
+      phone: "51 44444-4444",
+      isFavorite: true),
+  Contact(
+      name: "Lucas heineken",
+      photo: "assets/images/Lucas.jpg",
+      email: "lucas@gmail.com",
+      phone: "51 55555-5555"),
+  Contact(
+      name: "Marina te Empina",
+      photo: "assets/images/Marina.jpg",
+      email: "marina@gmail.com",
+      phone: "51 66666-6666",
+      isFavorite: true),
+  Contact(
+      name: "Paulo dos Broxa",
+      photo: "assets/images/Paulo.jpg",
+      email: "paulo@gmail.com",
+      phone: "51 77777-7777",
+      isFavorite: true),
+  Contact(
+      name: "Thomas Turbando",
+      photo: "assets/images/Thomas.jpg",
+      email: "thomas@gmail.com",
+      phone: "51 88888-8888"),
+];
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,6 @@
+import 'package:agenda_de_contatos/models/contact.dart';
+import 'package:agenda_de_contatos/provider/listOfContacts.dart';
+import 'package:agenda_de_contatos/screens/home/details/details.dart';
 import 'package:agenda_de_contatos/sytle.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +22,7 @@ class _HomeState extends State<Home> {
       ),
       body: ListView.separated(
         itemBuilder: builder,
-        itemCount: 3,
+        itemCount: listOfContacts.length,
         separatorBuilder: (_, index) {
           return Divider();
         },
@@ -28,16 +31,31 @@ class _HomeState extends State<Home> {
   }
 
   Widget builder(BuildContext _, int index) {
+    Contact _contact = listOfContacts.elementAt(index);
     return ListTile(
       leading: IconButton(
-        icon: Icon(
-          Icons.star,
-          color: blueTheme,
-        ),
-        onPressed: () {},
+        icon: (_contact.isFavorite)
+            ? Icon(
+                Icons.star,
+                color: blueTheme,
+              )
+            : Icon(Icons.star_outline, color: blueTheme),
+        onPressed: () {
+          setState(() {
+            _contact.isFavorite = !_contact.isFavorite;
+          });
+        },
       ),
       trailing: IconButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext _) {
+                return Details(_contact);
+              },
+            ),
+          );
+        },
         icon: Icon(
           Icons.chevron_right,
           color: grayTheme,
@@ -45,12 +63,15 @@ class _HomeState extends State<Home> {
       ),
       title: Row(
         children: [
-          ClipOval(
-            child: Image.asset(
-              "assets/images/Nicole,jpg",
-              width: 40,
-              height: 40,
-              fit: BoxFit.cover,
+          Hero(
+            tag: _contact.name,
+            child: ClipOval(
+              child: Image.asset(
+                _contact.photo,
+                width: 40,
+                height: 40,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           SizedBox(
@@ -60,14 +81,14 @@ class _HomeState extends State<Home> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "Nicole",
+                _contact.name,
                 style: TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.bold,
                     color: grayTheme),
               ),
               Text(
-                "51 11111-1111",
+                _contact.phone,
                 style: TextStyle(
                   fontSize: 10,
                   color: grayTheme,
